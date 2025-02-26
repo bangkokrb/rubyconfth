173193d66475f0f294ff452a706cc21e78f4f80d
Matt Mayer <matt@matthewmayer.co.uk>
2025-02-26 20:07:48 +0700

    remove old site and add new static site

--- frontend/javascript/application.js
+++
@@ -1,7 +0,0 @@
-import "../stylesheets/application.scss"
-
-// Import all JavaScript & CSS files from src/_components
-import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
-
-import 'initializers/';
-import 'screens/';
--- frontend/javascript/initializers/stimulus.js
+++
@@ -1,8 +0,0 @@
-import { Application } from '@hotwired/stimulus';
-import NavigationController from '../controllers/navigation_controller';
-import ScheduleController from '../controllers/schedule_controller';
-
-window.Stimulus = Application.start();
-
-Stimulus.register('navigation', NavigationController);
-Stimulus.register('schedule', ScheduleController)
